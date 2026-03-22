4d226e6a58ab2d890d71be14db08b4f4f2672686
bhushan patil <bhushanpatil659145@gmail.com>
2026-03-22 15:10:57 +0530

    feat : Dashboard UI and dumy subjects

---
+++ app/src/main/java/com/example/prepdeck/domain/model/Subject.kt
@@ -0,0 +1,18 @@
+package com.example.prepdeck.domain.model
+
+import androidx.compose.ui.graphics.Color
+import com.example.prepdeck.ui.theme.gradient1
+import com.example.prepdeck.ui.theme.gradient2
+import com.example.prepdeck.ui.theme.gradient3
+import com.example.prepdeck.ui.theme.gradient4
+import com.example.prepdeck.ui.theme.gradient5
+
+data class Subject(
+    val name: String,
+    val goalHourse: Float,
+    val colors: List<Color>
+) {
+    companion object{
+        val subjectCardColors = listOf(gradient1, gradient2, gradient3, gradient4, gradient5)
+    }
+}
---
+++ app/src/main/java/com/example/prepdeck/presentation/dashboard/DashboardScreen.kt
@@ -0,0 +1,184 @@
+package com.example.prepdeck.presentation.dashboard
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.prepdeck.R
+import com.example.prepdeck.domain.model.Subject
+import com.example.prepdeck.presentation.dashboard.components.CountCard
+import com.example.prepdeck.presentation.dashboard.components.SubjectCard
+
+@Composable
+fun DashboardScreen( ) {
+
+    val subjects = listOf(
+        Subject(name = "English", goalHourse = 10f, colors = Subject.subjectCardColors[0]),
+        Subject(name = "Physics", goalHourse = 10f, colors = Subject.subjectCardColors[1]),
+        Subject(name = "Maths", goalHourse = 10f, colors = Subject.subjectCardColors[2]),
+        Subject(name = "Geology", goalHourse = 10f, colors = Subject.subjectCardColors[3]),
+        Subject(name = "Fine Arts", goalHourse = 10f, colors = Subject.subjectCardColors[4]),
+    )
+
+    Scaffold(
+        topBar = { DashboardScreenTopBar() }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            item {
+                CountCardSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    subjectCount = 5,
+                    studiedHours = "10",
+                    goalHours = "15"
+                )
+            }
+            item {
+                SubjectCardSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    subjectList = subjects
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DashboardScreenTopBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "PrepDeck",
+                style = MaterialTheme.typography.headlineMedium,
+            )
+        }
+    )
+}
+
+@Composable
+private fun CountCardSection(
+    modifier: Modifier,
+    subjectCount: Int,
+    studiedHours: String,
+    goalHours: String
+) {
+    Row(
+        modifier = modifier
+    ) {
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Subject Count",
+            count = "$subjectCount"
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Studied Hours",
+            count = studiedHours
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Goal Study Hours",
+            count = goalHours
+        )
+    }
+}
+
+@Composable
+private fun SubjectCardSection(
+    modifier: Modifier,
+    subjectList: List<Subject>,
+    emptyListText: String = "You don't have any subjects. \nClick the + button to add new subject"
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "SUBJECTS",
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(start = 12.dp)
+            )
+
+            IconButton(
+                onClick = { }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add subject"
+                )
+            }
+        }
+        if (subjectList.isEmpty()) {
+            Image(
+                modifier = Modifier
+                    .size(120.dp)
+                    .align ( Alignment.CenterHorizontally),
+                painter = painterResource(R.drawable.img_books),
+                contentDescription = emptyListText
+            )
+
+            Text(
+                modifier = modifier.fillMaxWidth(),
+                text = emptyListText,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray,
+                textAlign = TextAlign.Center
+            )
+        }
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(start = 12.dp, end = 12.dp)
+        ) {
+            items(subjectList) { subject->
+                SubjectCard(
+                    subjectName = subject.name,
+                    gradientColors = subject.colors,
+                    onClick = {  }
+                )
+
+            }
+        }
+    }
+}
